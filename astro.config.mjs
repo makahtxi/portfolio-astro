8b5c71c60f843a51786807741f797e6af4749a3c
pedrobotica <pebotica@gmail.com>
2024-07-01 16:31:15 +0100

    Project pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
-
 import { loadEnv } from "vite";
-const { SECRET_PASSWORD } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+import vercel from "@astrojs/vercel/serverless";
+const {
+  SECRET_PASSWORD
+} = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel()
 });
